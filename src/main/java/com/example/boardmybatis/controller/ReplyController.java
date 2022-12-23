42599fbb8a37e7f3bc36b66c1f9cbcdbbf3b86e8
myandue <97776790+myandue@users.noreply.github.com>
2022-12-23 12:48:16 +0900

    22.12.23

--- src/main/java/com/example/boardmybatis/controller/ReplyController.java
+++ src/main/java/com/example/boardmybatis/controller/ReplyController.java
@@ -17,71 +17,73 @@ import java.util.List;
 public class ReplyController {
 
     private final ReplyService replyService;
-//
-//    @PostMapping("")
-//    private String uploadReply(Model model,
-//                               @RequestBody Reply reply,
-//                               @SessionAttribute(name="loginUser", required = false) User loginUser){
-//        if(loginUser==null){
-//            model.addAttribute("msg", "Only user have access.");
-//            model.addAttribute("searchUrl", "")
-//        }
-//    }
 
-    //Reply Register
     @PostMapping("")
-    public ResponseEntity<String> register(@RequestBody Reply reply) {
-        System.out.println("post mapping reply = " + reply);
-        ResponseEntity<String> entity = null;
-        try {
-            replyService.save(reply);
-            entity = new ResponseEntity<String>("regSuccess", HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    private List<Reply> uploadReply(Model model,
+                               @PathVariable("id") int articleId,
+                               @RequestBody Reply reply,
+                               @SessionAttribute(name="loginUser", required = false) User loginUser){
+        if(loginUser==null){
         }
-        return entity;
+        reply.setArticleId(articleId);
+        replyService.save(reply);
+        return replyService.listAll(articleId);
     }
 
-    //Reply List
-    @GetMapping("/all/{articleId}")
-    public ResponseEntity<List<Reply>> list(@PathVariable("articleId") int articleId) {
-        ResponseEntity<List<Reply>> entity = null;
-        try {
-            entity = new ResponseEntity<List<Reply>>(replyService.listAll(articleId), HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            entity = new ResponseEntity<List<Reply>>(HttpStatus.BAD_REQUEST);
-        }
-        return entity;
-    }
-
-    //Reply Modify
-    @RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
-    public ResponseEntity<String> update(@PathVariable("id") int id, @RequestBody Reply reply) {
-        ResponseEntity<String> entity = null;
-        try {
-            reply.setId(id);
-            replyService.edit(reply);
-            entity = new ResponseEntity<String>("modSuccess", HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return entity;
-    }
-
-    //Reply Delete
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable("id") int id) {
-        ResponseEntity<String> entity = null;
-        try {
-            replyService.delete(id);
-            entity = new ResponseEntity<String>("delSuccess", HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return entity;
-    }
+//    //Reply Register
+//    @PostMapping("")
+//    public ResponseEntity<String> register(@RequestBody Reply reply) {
+//        System.out.println("post mapping reply = " + reply);
+//        ResponseEntity<String> entity = null;
+//        try {
+//            replyService.save(reply);
+//            entity = new ResponseEntity<String>("regSuccess", HttpStatus.OK);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//        return entity;
+//    }
+//
+//    //Reply List
+//    @GetMapping("/all/{articleId}")
+//    public ResponseEntity<List<Reply>> list(@PathVariable("articleId") int articleId) {
+//        ResponseEntity<List<Reply>> entity = null;
+//        try {
+//            entity = new ResponseEntity<List<Reply>>(replyService.listAll(articleId), HttpStatus.OK);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            entity = new ResponseEntity<List<Reply>>(HttpStatus.BAD_REQUEST);
+//        }
+//        return entity;
+//    }
+//
+//    //Reply Modify
+//    @RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+//    public ResponseEntity<String> update(@PathVariable("id") int id, @RequestBody Reply reply) {
+//        ResponseEntity<String> entity = null;
+//        try {
+//            reply.setId(id);
+//            replyService.edit(reply);
+//            entity = new ResponseEntity<String>("modSuccess", HttpStatus.OK);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//        return entity;
+//    }
+//
+//    //Reply Delete
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<String> delete(@PathVariable("id") int id) {
+//        ResponseEntity<String> entity = null;
+//        try {
+//            replyService.delete(id);
+//            entity = new ResponseEntity<String>("delSuccess", HttpStatus.OK);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//        return entity;
+//    }
 }
